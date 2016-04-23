db010b4e96c3231d78779a83f80ece82eb07fba7
Antti Leinonen <antti.leinonen@helsinki.fi>
2016-04-23 20:49:11 +0300

    1 tehtävän palautus

--- viikko5/LyyrakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
+++ viikko5/LyyrakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
@@ -45,4 +45,19 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void lataaKortilleJosPositiivinenRahamaara() {
+        kassa.lataa(kortti, 5);
+        
+        verify(kortti, times(1)).lataa(5);
+        verify(kortti).lataa(eq(5));
+    }
+    
+    @Test
+    public void eiLataaKortilleJosNegatiivinenRahamaara() {
+        kassa.lataa(kortti, -5);
+        
+        verify(kortti, times(0)).lataa(anyInt());
+    }
 }
